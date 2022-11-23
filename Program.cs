9ba078d12f3af50a02f427f949f36ad784c5b9d1
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-11-23 12:09:24 -0300

    Creando un nuevo middleware

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using webapi.middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,7 +22,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseWelcomePage();
+//app.UseWelcomePage();
+
+app.UseTimeMiddleware();
 
 app.MapControllers();
 
---
+++ middlewares/TimeMiddleware.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.middlewares
+{
+    public class TimeMiddleware
+    {
+        readonly RequestDelegate next;
+
+        public TimeMiddleware(RequestDelegate nextRequest)
+        {
+            next = nextRequest;
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.Query.Any(p => p.Key == "time"))
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+            }
+
+            await next(context);
+        }
+    }
+
+    public static class TimeMiddlewareExtension
+    {
+        public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimeMiddleware>();
+        }
+    }
+
+}
