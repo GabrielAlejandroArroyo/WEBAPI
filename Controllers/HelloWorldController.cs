1d0c0a6b8bb06b0c31d7d1a8ff56a5e3c25f1a1e
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-11-24 16:53:43 -0300

    Documentando API con Swagger

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -21,6 +21,7 @@ namespace webapi.Controllers
             helloWorldService = helloWorld;
         }
 
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok(helloWorldService.GetHelloWorld());
